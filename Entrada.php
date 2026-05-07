23316202e0526c792a8cf8f6cfbf3deeb25f5629
Felipe Cleverson <felipecleverson1@gmail.com>
2026-05-07 19:22:35 -0400

    saldo conta

--- Entrada.php
+++ Entrada.php
@@ -1,7 +1,41 @@
 <?php
     
-echo "Digite um numero: \n";
+$saldo = 1_0000;
+$titularConta = 'felipe';
 
-$numero = (float)fgets(STDIN);
+echo "******************************\n";
+echo "Titular: $titularConta\n";
+echo "Saldo atual: R$ $saldo\n";
+echo "******************************\n";
 
-echo " O numero e: $numero\n";
+echo "1. Consultar saldo atual\n";
+echo "2. Sacar valor\n";
+echo "3. Depositar valor\n";
+echo "4. Sair\n";
+
+
+$opcao = (int) fgets(STDIN);
+
+switch($opcao){
+    case 1:
+        echo "******************************\n";
+        echo "Titular: $titularConta\n";
+        echo "Saldo atual: R$ $saldo\n";
+        echo "******************************\n";
+    break;
+    case 2:
+        echo "******************************\n";
+        echo "Titular: $titularConta\n";
+    break;
+    case 3:
+        echo "******************************\n";
+        echo "Titular: $titularConta\n";
+        echo "Saldo atual: R$ $saldo\n";
+    break;
+      case 4:
+        echo "******************************\n";
+        echo "Qual  valor deseja sacar?\n";
+    break;
+    
+
+}
