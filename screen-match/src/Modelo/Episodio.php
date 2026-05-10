04fd0bca3dec8e4819b0fa12725d91638bcdb1fd
Felipe Cleverson <felipecleverson1@gmail.com>
2026-05-10 13:59:04 -0400

    curso_php

--- screen-match/src/Modelo/Episodio.php
+++ screen-match/src/Modelo/Episodio.php
@@ -2,7 +2,7 @@
 
 class Episodio implements Avaliavel{
 
-    private array $notas;
+    use ComAvaliacao;
 
     public function __construct(
         public readonly Serie $serie,
@@ -10,20 +10,8 @@ class Episodio implements Avaliavel{
         public readonly int $numero,
     )
     {
-       $this->notas = [];
     }
 
-    public function avalia(float $nota): void
-    {
-        $this->notas[] = $nota;
-    }
-
-    public function media(): float
-    {
-        $somaNotas = array_sum($this->notas);
-        $quantidadeNotas = count($this->notas);
-
-        return $somaNotas / $quantidadeNotas;
-    }
+   
 
 }
