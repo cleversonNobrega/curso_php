8229e208f2aead67c50b234848a662e29a0f23b5
Felipe Cleverson <felipecleverson1@gmail.com>
2026-05-10 21:38:53 -0400

    curso.php

---
+++ Manipulacao-De-String/regex.php
@@ -0,0 +1,16 @@
+<?php
+
+$telefones = ['(24) 99999 - 9999 ', ' (21) 99999 - 9999 ', '(24) 2222 - 2222'];
+
+foreach($telefones as $telefone){
+    $telefoneValido = preg_match(
+       '[0-9]{2}\) 9?[0-9]{4} - [0-9]{4}/'
+    );
+
+
+    if($telefoneValido){
+        echo 'telefone valido' . PHP_EOL;
+    }else {
+        echo 'telefone invalido' . PHP_EOL;
+    }
+}
