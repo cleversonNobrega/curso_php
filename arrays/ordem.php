68186baaea8d3d245225853bbb2d486aaf45bdaf
Felipe Cleverson <felipecleverson1@gmail.com>
2026-05-11 23:04:15 -0400

    curso.php

--- arrays/ordem.php
+++ arrays/ordem.php
@@ -17,18 +17,9 @@ $notas = [
 
 
 function orderNotas(array $nota1, $nota2) : int {
-    if($nota1['nota'] > $Nnota2['nota'] ){
-        return -1;
-    }
-
-    if($nota2['nota']  > $nota1['nota'] ){
-        return 1;
-    }
-
-    return 0;
+    return $nota2['nota'] <=> $nota1['nota'];
 }
 
 
 usort($notas ,'orderNotas');
-
 var_dump($notas);
