be437e472c298761e28076a0009cc1bf7a6de4a2
Felipe Cleverson <felipecleverson1@gmail.com>
2026-05-13 22:37:25 -0400

    curso_php

--- tipagem/tipagem1.php
+++ tipagem/tipagem1.php
@@ -3,3 +3,18 @@
 $string = 'valor textuais';
 $int = 27;
 $float = 15.5;
+$bool = false;
+
+$array = [$string, $int, $float];
+
+$valorNumerico = '27';
+$valorInteiro = (int) $valorNumerico;
+
+$valorDecimal = 27.5;
+$valorInteiro = (int) $valorDecimal;
+
+var_dump($valorInteiro);
+var_dump((bool) '');
+
+
+var_dump((int) '27 bananas');
